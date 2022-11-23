0961fd15802093c80b30b8a687cc806eae6da3f8
unknown <tristengeven@gmail.com>
2022-11-23 09:43:44 -0600

    loading message

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -140,6 +140,12 @@ export default function Game() {
         </div>
       ); // end question board
     default:
-      return <div>Loading...</div>;
+      return (
+        <div className="bg-blue-800 h-screen overflow-hidden">
+          <h1 className="text-white font-bold text-center text-6xl pt-24">
+            Loading...
+          </h1>
+        </div>
+      );
   }
 }
